28099bed78999223b6b4d6e8e644b6c50b0ab0e8
Audrey Dutcher <audrey@rhelmot.io>
2023-09-21 17:46:51 -0700

    Cross fingers for successful deployment

---
+++ deploy/deploy.sh
@@ -0,0 +1,6 @@
+INSTALL="lunchables"
+NAMESPACE="rhelmot"
+
+ROOTDIR="$(realpath $(dirname $(realpath $0))/..)"
+CHART="$ROOTDIR/chart/lunchables"
+helm upgrade "$INSTALL" "$CHART" --install --namespace "$NAMESPACE" -f "$ROOTDIR/deploy/connections.yaml" -f "$ROOTDIR/deploy/resources.yaml" -f "$ROOTDIR/deploy/images.yaml" -f "$ROOTDIR/deploy/secrets.yaml"
