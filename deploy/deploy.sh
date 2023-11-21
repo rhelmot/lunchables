83ec6a9da652a9f2ef44fea1118606736fa8c2d6
Audrey Dutcher <audrey@rhelmot.io>
2023-11-21 15:03:05 -0700

    Adjust sa permissions to be more standard

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 INSTALL="lunchables"
 NAMESPACE="rhelmot"
 
-ROOTDIR="$(realpath $(dirname $(realpath $0))/..)"
+ROOTDIR="$(realpath "$(dirname "$(realpath "$0")")"/..)"
 CHART="$ROOTDIR/chart/lunchables"
 helm dependency build "$CHART"
 helm upgrade "$INSTALL" "$CHART" --install --namespace "$NAMESPACE" -f "$ROOTDIR/deploy/connections.yaml" -f "$ROOTDIR/deploy/resources.yaml" -f "$ROOTDIR/deploy/images.yaml" -f "$ROOTDIR/deploy/secrets.yaml" "$@"
